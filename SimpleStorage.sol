90321da64220f0593bbba2135c2b34e6c37a3402
kash-cpu <abdulazeez.kasheem@gmail.com>
2025-03-12 11:20:09 +0100

    Basic types in solidity

---
+++ SimpleStorage.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.8.18 // This is the solidity version
+contract SimplsStorage {
+    //Basic Types: boolean, int, uint, address, bytes, string
+    bool myBool = tue;
+    int64 myInt = -100;
+    uint64 myUint = 100;
+    address myAddress = 0x1234567890123456789012345678901234567890;
+    bytes32 myBytes = "Hello World";
+    string myString = "Hello World";
+    //Composite Types: array, struct, mapping, enum
+}
